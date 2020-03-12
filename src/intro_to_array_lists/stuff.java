27fd268747b6baa63bb4ca8807c5ca40b815c83a
Jack <jackgorton57@gmail.com>
2020-03-11 21:51:19 -0700

    Create stuff.java

---
+++ src/intro_to_array_lists/stuff.java
@@ -0,0 +1,14 @@
+package intro_to_array_lists;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class stuff {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+
+
+
+
+
+}
